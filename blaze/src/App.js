717e1eccd0bbcf97a36d6f529ee86c0187c5d35f
Sadie109 <101196558+Sadie109@users.noreply.github.com>
2023-02-10 13:53:49 +0000

    setting up UI library and imports, with api key and user code

--- blaze/src/App.js
+++ blaze/src/App.js
@@ -1,5 +1,6 @@
-import { StreamChat } from "stream-chat";
-import  {
+import React, { useEffect, useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import {
   Chat,
   Channel,
   ChannelHeader,
@@ -9,20 +10,54 @@ import  {
   Thread,
   Window,
 } from 'stream-chat-react';
+import '@stream-io/stream-chat-css/dist/css/index.css';
 
-const client = new StreamChat('your_api_key');
+const filters = { type: 'gaming' };
+const options = { state: true, presence: true, limit: 10 };
+const sort = { last_message_at: -1 };
 
-const App = () => (
-  <Chat client={client}>
-    <Channel channel={channel}>
-      <Window>
-        <ChannelHeader />
-        <MessageList />
-        <MessageInput />
-      </Window>
-      <Thread />
-    </Channel>
-  </Chat>
+const App = () => {
+const [client, setClient] = useState(null);
+
+useEffect(() => {
+const newClient = new StreamChat('2sfh4x6fvfs7');
+
+    const handleConnectionChange = ({ online = false }) => {
+      if (!online) return console.log('connection lost');
+      setClient(newClient);
+    };
+
+    newClient.on('connection.changed', handleConnectionChange);
+
+    newClient.connectUser(
+      {
+        id: 'dave-matthews',
+        name: 'Dave Matthews',
+      },
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGF2ZS1tYXR0aGV3cyJ9.-d0UZ9n1gwsiKUSVzZ1x5Eds8sFZziOXHsBTvbcjhgg',
+    );
+
+    return () => {
+      newClient.off('connection.changed', handleConnectionChange);
+      newClient.disconnectUser().then(() => console.log('connection closed'));
+    };
+}, []);
+
+if (!client) return null;
+
+return (
+<Chat client={client}>
+<ChannelList filters={filters} sort={sort} options={options} />
+<Channel>
+<Window>
+<ChannelHeader />
+<MessageList />
+<MessageInput />
+</Window>
+<Thread />
+</Channel>
+</Chat>
 );
+};
 
-export default App
+export default App;
