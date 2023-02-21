c44867961a3233d43bd2db782d78c80e750e52e6
Sadie109 <101196558+Sadie109@users.noreply.github.com>
2023-02-21 17:33:24 +0000

    set up error useState for password check

--- blaze/src/components/Auth.js
+++ blaze/src/components/Auth.js
@@ -1,21 +1,49 @@
-import {useState} from "react";
+import { useState } from "react";
 
 const Auth = () => {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(null);
+  const [error, setError] = useState(false);
+  console.log(password);
+  console.log(username);
+  console.log(confirmPassword);
 
-    const [username, setUsername] = useState(null);
+  const handleSubmit = () => {
+    console.log("submitted")
+    if (password !== confirmPassword) {
+        setError(true)
+        return
+    }
+  }
 
-    console.log(username)
   return (
     <div className="auth-container">
       <div className="auth-container-box">
         <div className="auth-container-form">
-            <input
+          <input
             type="text"
             id="username"
             name="username"
             placeholder="username"
             onChange={(e) => setUsername(e.target.value)}
-            />
+          />
+          <input
+            type="text"
+            id="password"
+            name="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="text"
+            id="password-check"
+            name="password-check"
+            placeholder="confirm password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          {error && <p> Incorrect Password </p>}
+          <button onClick={handleSubmit}>GO</button>
         </div>
       </div>
     </div>
