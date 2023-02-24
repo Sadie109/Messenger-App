38d2c34081b5377bf0f0ad08302b50174f221999
Sadie109 <101196558+Sadie109@users.noreply.github.com>
2023-02-24 16:49:59 +0000

    finished signing up credentials for backend and front end

--- server/index.js
+++ server/index.js
@@ -1,18 +1,36 @@
 const PORT = 8000;
 const express = require("express");
 const app = express();
-const cors = require('cors')
-app.use(cors())
-app.use(express.json())
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const { v1: uuidv1 } = require("uuid");
+const { connect } = require("getstream");
 
-app.post('/signup', async (req,res) =>{
-    try {
-         const {username, password} = req.body
-         console.log(username, password)
-    } catch (error) {
-    console.log(error)
-    res, status(500).json({message:error})
-    }
+app.use(cors());
+app.use(express.json());
+
+const API_KEY = "2sfh4x6fvfs7";
+const API_SECRET =
+  "2muur4vvms4jz5c8j2nrtkaxhe9fwa5g9egh9nexkekhyby4aema5anx2xjtte5b";
+const APP_ID = "1233704";
+
+app.post("/signup", async (req, res) => {
+  try {
+    console.log(req);
+    const { username, password } = req.body;
+
+    const userID = uuidv1();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const client = connect(API_KEY, API_SECRET, APP_ID);
+    const token = client.createUserToken(userID)
+
+    res.status(200).json({ username, password, userID, hashedPassword, token})
+
+    console.log(username, password);
+  } catch (error) {
+    console.log(error);
+    res, status(500).json({ message: error });
+  }
 });
 
 app.listen(PORT, () => console.log("server running on PORT" + PORT));
